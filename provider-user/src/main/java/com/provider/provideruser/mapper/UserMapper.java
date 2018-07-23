5d74ceb922f4ffd2d0c7713555ade02b63f52195
lhl <703646566@qq.com>
2018-07-23 15:54:45 +0800

    New Pro

---
+++ provider-user/src/main/java/com/provider/provideruser/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.provider.provideruser.mapper;
+
+import com.provider.provideruser.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ provider-user/src/main/java/com/provider/provideruser/model/User.java
@@ -0,0 +1,45 @@
+package com.provider.provideruser.model;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    private Date submissionDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Date getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(Date submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+}
